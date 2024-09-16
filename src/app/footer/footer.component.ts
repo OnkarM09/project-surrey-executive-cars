bae79adc5fcb8eae84bf1bfe9ad19bd5b9c17eb6
Onkar Meherwade <onkar.meherwade@9ine.com>
2024-09-16 14:43:00 +0530

    moved common data to home component.ts file and responsive styling added for carousel

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+
+  facebookLink : string = 'https://www.facebook.com/surreyexecutivecars/';
+
+  footerServicesArr : any[] = [
+    {
+      icon : 'bi-taxi-front',
+      text : 'Local Taxi Services'
+    },
+    {
+      icon : 'bi-airplane',
+      text : 'Airport Transfers'
+    },
+    {
+      icon : 'bi-person',
+      text : 'Corporate Trasnsportation'
+    },
+    {
+      icon : 'bi-calendar2-event',
+      text : 'Event Transportation'
+    },
+  ];
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AnimateOnScrollModule } from 'primeng/animateonscroll';
+
+interface SafetyInfo {
+  imagePath: string,
+  headerTxt: string,
+  infoTxt: string,
+  altTxt : string
+}
 
 @Component({
   selector: 'app-home',
@@ -10,6 +17,35 @@ import { AnimateOnScrollModule } from 'primeng/animateonscroll';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  safetyInfoArr: SafetyInfo[] = [];
+  iconPath : string  = '../../assets/Images/icons/';
+  ngOnInit(): void {
+    this.safetyInfoArr = [
+      {
+        imagePath : `${this.iconPath}car-insurance.png`,
+        headerTxt : 'Safety First',
+        infoTxt : 'Your safety is our top priority. We implement rigorous safety protocols to ensure a secure and comfortable ride every time.',
+        altTxt : 'car-insurance.png'
+      },
+      {
+        imagePath : `${this.iconPath}money.png`,
+        headerTxt : 'Reasonable Rates',
+        infoTxt : 'We pride ourselves on providing affordable taxi services without compromising on quality.',
+        altTxt : 'money.png'
+      },
+      {
+        imagePath : `${this.iconPath}bus.png`,
+        headerTxt : 'Largest Fleet',
+        infoTxt : 'We opertate a large fleet of well-maintained vehicles to meet all your transportation needs.',
+        altTxt : 'bus.png'
+      },
+      {
+        imagePath : `${this.iconPath}24-7.png`,
+        headerTxt : '24/7 Service',
+        infoTxt : 'We are proud to offer 24/7 taxi service to ensure that you have reliable transportation whenever you need it.',
+        altTxt : '24-7.png'
+      },
+    ];
+  }
 }
