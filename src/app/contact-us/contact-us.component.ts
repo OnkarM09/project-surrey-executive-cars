b90212fb4ef8d4c27bcaf16cf6244d09a96bdcab
Onkar Meherwade <onkar.meherwade@9ine.com>
2024-09-17 15:03:40 +0530

    integreated email js with recaptcha validation

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RecaptchaFormsModule, RecaptchaModule } from 'ng-recaptcha';
 import { EditorModule } from 'primeng/editor';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact-us',
@@ -15,11 +16,29 @@ export class ContactUsComponent {
 
   captcha : string | null = '';
   email : string = 'test1@email.com';
-  text: string | undefined;
+  message: string | undefined;
+
+  constructor(){
+  }
 
   resolvedCaptcha(captcharesponse : string | null){
     this.captcha = captcharesponse;
     console.log(this.captcha);
+  }
+
+  sendEmail(e:Event){
+    emailjs
+      .sendForm('service_9za1jud', 'template_q50wuiy', e.target as HTMLFormElement, {
+        publicKey: 'C7GiovA5xz-EE_JGH',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', (error as EmailJSResponseStatus).text);
+        },
+      );
   }
 
 }
