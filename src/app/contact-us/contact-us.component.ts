9a7a65ddd9c51cd373745d9547c9b28aa78f00e8
Onkar Meherwade <onkar.meherwade@9ine.com>
2024-09-17 12:33:44 +0530

    Integrated Recaptcha

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RecaptchaFormsModule, RecaptchaModule } from 'ng-recaptcha';
+import { RecaptchaCommonModule } from 'ng-recaptcha/lib/recaptcha-common.module';
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, RecaptchaFormsModule, RecaptchaModule],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss'
 })
 export class ContactUsComponent {
+
+  captcha : string | null = '';
+  email : string = 'test1@email.com';
+
+  resolvedCaptcha(captcharesponse : string | null){
+    this.captcha = captcharesponse;
+    console.log(this.captcha);
+  }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,7 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+
+  //6Ld-e0YqAAAAAH7N1rd2GIjtsMIR_IDEQFhgdH62 --- site key
+  //6Ld-e0YqAAAAABu1L6uggBxTPUBREww1283TlHbO --- secret key
