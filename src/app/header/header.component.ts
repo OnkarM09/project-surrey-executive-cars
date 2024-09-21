2e5629800f241d10a6da6ee9f84e0f08c54d27a9
Onkar Meherwade <onkar.meherwade@9ine.com>
2024-09-21 19:07:33 +0530

    navbar collapse issue and footer gap issue for min width 992px is resolved

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MenuItem } from 'primeng/api';
@@ -6,13 +7,21 @@ import { MenubarModule } from 'primeng/menubar';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MenubarModule, RouterModule],
+  imports: [CommonModule ,MenubarModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent implements OnInit {
-  items : MenuItem[] | undefined;
+  items: MenuItem[] | undefined;
+  isCollapsed: boolean = false;
   ngOnInit() {
-}
+  }
+
+  hideBootstrapNavCollapse(){
+    const navbar = document.querySelector('.navbar-collapse');
+    if(navbar != null){
+      navbar.classList.remove('show');
+    }
+  }
 
 }
